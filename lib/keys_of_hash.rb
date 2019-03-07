20e8091a245cac5af877b5fec30ff8caff075056
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-07 23:42:36 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,14 +1,13 @@
 class Hash
   def keys_of(arguments)
     listOfKeys=[]
-
-     arguments.each do |arg|
-        map do |key,value|
-             if arg == value
-       listOfKeys  << key
-          end
-         end
-        end
-    listOfKeys
-      end
-    end
+     arguments[:animals].each do |animal, country|
+       if country == argument 
+         listOfKeys << animal 
+         
+     end 
+   end 
+   listOfKeys
+ end 
+       
+      

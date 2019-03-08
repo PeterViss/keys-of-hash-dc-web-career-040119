666a6d0bf4de3f2601bb6b01651e4c2cc3e8aca9
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-08 00:05:51 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(arguments)
     array = []
      arguments.each do |animals|
-       animals.each[] do |animal, country|
+       animals.each do |animal, country|
        if country == argument
          array << animal.split
        end
